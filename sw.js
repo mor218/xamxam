cf6a3d7bfdce6518b81f7ff8f1aa2f135e3c322b
Serigne Mor Gueye <moriste1972@gmail.com>
2026-03-06 23:52:14 +0000

    Mise à jour site XAM XAM

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10692275
+    "domain": "3nbf4.com",
+    "zoneId": 10692377
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
